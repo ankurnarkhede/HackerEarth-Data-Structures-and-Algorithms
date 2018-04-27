27b4a390c095700cdef0c5fe573dca88bfb046da
smartankur4u <ankurnarkhede@gmail.com>
2018-04-27 22:37:34 +0530

    more programs solved...

--- ziguar-assign-1-round-robin.py
+++ ziguar-assign-1-round-robin.py
@@ -3,11 +3,39 @@ import sys
 try:
     fr = open ("inputfile1.txt", "r+")
     input_string = fr.read ().strip ().split (' ')
+    print (input_string)
 
     for i in range (0, len (input_string), +3):
+        print (i)
+        if (i == 0):
+            fo1 = open ("OutputFile1.txt", "w+")
+            fo2 = open ("OutputFile2.txt", "w+")
+            fo3 = open ("OutputFile3.txt", "w+")
 
+            fo1.write (input_string[i] + ' ')
+            fo2.write (input_string[i + 1] + ' ')
+            fo3.write (input_string[i + 2] + ' ')
 
+            fo1.close ()
+            fo2.close ()
+            fo3.close ()
+        else:
+            fo1 = open ("OutputFile1.txt", "a")
+            fo2 = open ("OutputFile2.txt", "a")
+            fo3 = open ("OutputFile3.txt", "a")
+
+            try:
+                fo1.write (input_string[i] + ' ')
+                fo2.write (input_string[i + 1] + ' ')
+                fo3.write (input_string[i + 2] + ' ')
+
+                fo1.close ()
+                fo2.close ()
+                fo3.close ()
+
+            except(IndexError):
+                break
 
 
 except(FileNotFoundError):
-    print ('File doesnt exixts')
+    print ('File doesnt exists')
