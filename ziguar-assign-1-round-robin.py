13551bb36e10b38b607195dc98fe3ca6434cbdda
smartankur4u <ankurnarkhede@gmail.com>
2018-04-27 22:56:08 +0530

    more programs solved...

--- ziguar-assign-1-round-robin.py
+++ ziguar-assign-1-round-robin.py
@@ -1,13 +1,29 @@
 import sys
 
+flag = 0
 try:
     fr = open ("inputfile1.txt", "r+")
     input_string = fr.read ().strip ().split (' ')
     print (input_string)
 
+    fr1 = open ("OutputFile1.txt", "r+")
+    fr2 = open ("OutputFile2.txt", "r+")
+    fr3 = open ("OutputFile3.txt", "r+")
+
+    input_string1 = fr1.read ().strip ().split (' ')
+    input_string2 = fr2.read ().strip ().split (' ')
+    input_string3 = fr3.read ().strip ().split (' ')
+
+    if (input_string1 != input_string2 != input_string3):
+        start = min (input_string3, input_string2, input_string1)
+        flag = 1
+        print ('start=', start)
+
+
     for i in range (0, len (input_string), +3):
         print (i)
-        if (i == 0):
+
+        if (i == 0 and flag == 0):
             fo1 = open ("OutputFile1.txt", "w+")
             fo2 = open ("OutputFile2.txt", "w+")
             fo3 = open ("OutputFile3.txt", "w+")
@@ -19,6 +35,7 @@ try:
             fo1.close ()
             fo2.close ()
             fo3.close ()
+
         else:
             fo1 = open ("OutputFile1.txt", "a")
             fo2 = open ("OutputFile2.txt", "a")
