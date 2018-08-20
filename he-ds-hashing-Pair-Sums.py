8fd6114268f6d985f067aa7ce3d4df5242c6a928
Ankur Narkhede <ankurnarkhede@gmail.com>
2018-08-21 00:15:38 +0530

    more programs solved...

---
+++ he-ds-hashing-Pair-Sums.py
@@ -0,0 +1,14 @@
+# https://www.hackerearth.com/practice/data-structures/hash-tables/basics-of-hash-tables/practice-problems/algorithm/pair-sums/
+# 5 9
+# 1 2 3 4 5
+
+import sys
+
+
+def main():
+    n, k = (map (int, sys.stdin.readline ().strip ().split (' ')))
+    a = (list (map (int, sys.stdin.readline ().strip ().split (' '))))
+
+
+if __name__ == '__main__':
+    main ()
